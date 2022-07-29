6b80a216248682a2a0b06279c4631d970a131972
asadtariq-dev <asad.tariq@devsinc.com>
2022-07-29 15:43:17 +0500

    Used 'before_save' Callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,26 @@ class User < ApplicationRecord
 
   after_validation :normalize_username_and_email
 
+  before_save :validate_before_save
+
   private
 
   # if username is not given it will get one from the email
   def ensure_username_has_value
-    puts 'before validation'
+    puts 'before validation.......'
     self.username = email.split('@')[0] if username.nil? && !email.blank?
     puts "#{username}\n#{email}"
   end
 
   def normalize_username_and_email
-    puts 'after validation'
+    puts 'after validation.......'
     self.username = username.downcase.titleize
     self.email = email.downcase
     puts "#{username}\n#{email}"
   end
+
+  def validate_before_save
+    puts 'before saving.....'
+  end
+
 end
