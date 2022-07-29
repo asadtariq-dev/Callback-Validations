04fe716c660eb53f7efab97d8f211792bc58c44c
asadtariq-dev <asad.tariq@devsinc.com>
2022-07-29 15:24:41 +0500

    Used 'after_validation' Callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,21 @@ class User < ApplicationRecord
 
   before_validation :ensure_username_has_value
 
+  after_validation :normalize_username_and_email
+
   private
 
   # if username is not given it will get one from the email
   def ensure_username_has_value
+    puts 'before validation'
     self.username = email.split('@')[0] if username.nil? && !email.blank?
+    puts "#{username}\n#{email}"
+  end
+
+  def normalize_username_and_email
+    puts 'after validation'
+    self.username = username.downcase.titleize
+    self.email = email.downcase
+    puts "#{username}\n#{email}"
   end
 end
