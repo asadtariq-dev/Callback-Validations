dec1861b6bf00f869da7b424ce7d2027eef837ae
asadtariq-dev <asad.tariq@devsinc.com>
2022-07-29 19:27:15 +0500

    Get params using route params

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,12 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
   def index
     @sum = params[:sum]
+  end
+
+  def show
+   @id = params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :clients
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :clients
+  get '/clients', to: 'clients#index'
+  get '/clients/:id', to: 'clients#show'
 
-  # Defines the root path route ("/")
-  # root "client#index"
 end
